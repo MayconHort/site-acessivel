554a9199244404e67416f99dd6d20a1b53fa75d0
MayconHort <maycon.hort@escola.pr.gov.br>
2025-07-30 13:36:13 -0300

    ...

---
+++ script.js
@@ -0,0 +1,6 @@
+function  inserirNome(){
+let nomeUsuario = prompt( "qual o seu nome");
+   let elememnto = document. querySelector("#nome-usuario");
+   console.log(elemento);
+   elemento.textcontent = nomeUsuario;
+}
